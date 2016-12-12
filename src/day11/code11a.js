a07807f2b77abed5e2716f78fd6d16ff8682a649
Heber Billings <hbillings@ancestry.com>
2016-12-12 14:05:07 -0700

    sad

--- src/day11/code11a.js
+++ src/day11/code11a.js
@@ -1,3 +1,7 @@
+let _ = require('lodash');
+
+// NOT WORKING!!!
+
 /*
  The first floor contains a strontium generator, a strontium-compatible microchip, a plutonium generator, and a plutonium-compatible microchip.
  The second floor contains a thulium generator, a ruthenium generator, a ruthenium-compatible microchip, a curium generator, and a curium-compatible microchip.
@@ -5,31 +9,50 @@
  The fourth floor contains nothing relevant.
  */
 
-let layout = [
-    {
-        name: 'H',
-        type: 'M',
-        floor: 1
-    },
-    {
-        name: 'L',
-        type: 'M',
-        floor: 1
-    },
-    {
-        name: 'H',
-        type: 'G',
-        floor: 2
-    },
-    {
-        name: 'L',
-        type: 'G',
-        floor: 3
-    },
+let items = [
+    { name: 'S', type: 'G', floor: 1 },
+    { name: 'S', type: 'M', floor: 1 },
+    { name: 'P', type: 'G', floor: 1 },
+    { name: 'P', type: 'M', floor: 1 },
+    { name: 'T', type: 'G', floor: 2 },
+    { name: 'T', type: 'M', floor: 3 },
+    { name: 'R', type: 'G', floor: 2 },
+    { name: 'R', type: 'M', floor: 2 },
+    { name: 'C', type: 'G', floor: 2 },
+    { name: 'C', type: 'M', floor: 2 },
 ];
 
+// let items = [
+//     { name: 'H', type: 'G', floor: 2 },
+//     { name: 'H', type: 'M', floor: 1 },
+//     { name: 'L', type: 'G', floor: 3 },
+//     { name: 'L', type: 'M', floor: 1 },
+// ];
+
+let ele = {
+    items: [],
+    floor: 1
+};
+
 function print() {
-    for (let i = 1; i <= 4; i++) {
-        let str = 'F' + i;
+    for (let i = 4; i >= 1; i--) {
+        let str = 'F' + i + ' ';
+
+        if (ele.floor === i) {
+            str += 'E ';
+        } else {
+            str += '. ';
+        }
+
+        _.forEach(items, (item) => {
+            if (item.floor === i) {
+                str += item.name + item.type + ' ';
+            } else {
+                str += '.  ';
+            }
+        });
+        console.log(str);
     }
 }
+
+print();
