07bbcbd0f8848deed46a2e2921aad9a7c637a1f2
Heber Billings <hbillings@ancestry.com>
2016-12-13 08:36:17 -0700

    day 12

--- src/day11/code11b.js
+++ src/day11/code11b.js
@@ -166,10 +166,18 @@ function searchForSolution(initialState) {
     throw new Error('No solution found')
 }
 
-// The first floor contains a strontium generator, a strontium-compatible microchip, a plutonium generator, and a plutonium-compatible microchip.
-// The second floor contains a thulium generator, a ruthenium generator, a ruthenium-compatible microchip, a curium generator, and a curium-compatible microchip.
-// The third floor contains a thulium-compatible microchip.
-// The fourth floor contains nothing relevant.
+/*
+The first floor contains a strontium generator, a strontium-compatible microchip, a plutonium generator, and a plutonium-compatible microchip.
+The second floor contains a thulium generator, a ruthenium generator, a ruthenium-compatible microchip, a curium generator, and a curium-compatible microchip.
+The third floor contains a thulium-compatible microchip.
+The fourth floor contains nothing relevant.
+
+Upon entering the isolated containment area, however, you notice some extra parts on the first floor that weren't listed on the record outside:
+An elerium generator.
+An elerium-compatible microchip.
+A dilithium generator.
+A dilithium-compatible microchip.
+ */
 const initialState = {
     elevator: 0,
     items: [
@@ -178,8 +186,8 @@ const initialState = {
         1, 2, // Thulium
         1, 1, // Ruthenium
         1, 1, // Curium
-        // 0, 0, // Elerium
-        // 0, 0, // Dilithium
+        0, 0, // Elerium
+        0, 0, // Dilithium
     ]
 }
 
---
+++ src/day12/code12b.js
@@ -0,0 +1,59 @@
+let _ = require('lodash');
+let fs = require('fs');
+require.extensions['.txt'] = function (module, filename) {
+    module.exports = fs.readFileSync(filename, 'utf8');
+};
+
+let input = require('./input.txt');
+// let input = require('./test.txt');
+
+let commands = input.split('\n');
+commands.pop();
+
+let index = 0;
+let register = {
+    a: 0,
+    b: 0,
+    c: 1,
+    d: 0,
+};
+
+function getValue(x) {
+    if (_.includes(_.keys(register), x)) {
+        return register[x];
+    } else {
+        return _.parseInt(x);
+    }
+}
+
+let code = {
+    cpy: (x, y) => {
+        register[y] = getValue(x);
+        index++;
+    },
+    inc: (x) => {
+        register[x]++;
+        index++;
+    },
+    dec: (x) => {
+        register[x]--;
+        index++;
+    },
+    jnz: (x, y) => {
+        if (getValue(x) !== 0) {
+            index += _.parseInt(y);
+        } else {
+            index++;
+        }
+    }
+};
+
+while (index < commands.length) {
+    let command = commands[index].split(' ');
+    // console.log('command', command);
+    code[command[0]](command[1], command[2]);
+    // console.log('register', register);
+}
+
+console.log('=======================');
+console.log('a:', register.a);
